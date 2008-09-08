d1e701c53a8e9baaa9dace24877f1a6be6a8f14c
Charlie Groves <charlie.groves@gmail.com>
2008-09-07 23:07:24 -0700

    Various snippet fixes
    * Fix a compiler warning by explicitly casing the JSON map
    * Actually put created Activity objects into the db by using
      transactions
    * Use a time-only date format for activities in the past day

--- webapp/src/main/scala/sevorg/tic/snippet/Tic.scala
+++ webapp/src/main/scala/sevorg/tic/snippet/Tic.scala
@@ -1,5 +1,6 @@
 package sevorg.tic.snippet
 
+import java.text.SimpleDateFormat
 import scala.xml.{Group, NodeSeq, Text}
 
 import net.liftweb.http.{JsonCmd, JsonHandler, S, SHtml}
@@ -13,14 +14,17 @@ import sevorg.tic.model.Model.{setToWrapper,listToWrapper}
 class Tic {
   object json extends JsonHandler {
     def apply(in: Any): JsCmd = JqJsCmds.PrependHtml("active", in match {
-        case JsonCmd("processForm", _, p: Map[String, String], _) => createActivity(p)
+        case JsonCmd("processForm", _, p: Map[k, v], _) => createActivity(p.asInstanceOf[Map[String, String]])
         case x => <tr><td>Didn't understand message! {x}</td></tr>
       })
   }
 
   private def createActivity(params: Map[String, String]) = {
+      val tran = Model.em.getTransaction()
+      tran.begin()
       val activity = new Activity(urlDecode(params("name")))
       Model.em.persist(activity)
+      tran.commit()
       makeActiveRow(activity)
   }
 
@@ -30,17 +34,22 @@ class Tic {
     )
 
   def active: NodeSeq = {
-      val activities = Model.em.createQuery("from Activity").getResultList.asInstanceOf[java.util.List[Activity]]
+      val activities = Model.em.createQuery("SELECT a FROM Activity a WHERE a.stop IS NULL").getResultList.asInstanceOf[java.util.List[Activity]]
       activities.flatMap(makeActiveRow)
   }
 
-  private def makeActiveRow(act: Activity) = bind("f",
-    <tr>
-      <td>
-        <f:name>Working on tic</f:name>
-      </td>
-      <td>
-        <f:start>10:30</f:start>
-      </td>
-    </tr>, "name" -> Text(act.getName), "start" -> Text(toInternetDate(act.getStart)))
+  val todayFormatter = new SimpleDateFormat("HH:mm")
+  val earlierFormatter = new SimpleDateFormat("MM/dd HH:mm")
+  private def makeActiveRow(act: Activity) = {
+    val formatter = if (System.currentTimeMillis - act.getStart.getTime < (1000 * 60 * 60 * 24)) todayFormatter else earlierFormatter
+    bind("f",
+      <tr>
+        <td>
+          <f:name>Working on tic</f:name>
+        </td>
+        <td>
+          <f:start>10:30</f:start>
+        </td>
+      </tr>, "name" -> Text(act.getName), "start" -> Text(formatter.format(act.getStart)))
+  }
 }
