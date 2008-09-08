000558ec390f97b77391bc8adf7b3159417e35bd
Charles Groves <groves@mort.local>
2008-09-07 17:31:27 -0700

    Basic activity persistent class

---
+++ persistence/src/main/java/sevorg/tic/model/Activity.java
@@ -0,0 +1,72 @@
+package sevorg.tic.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Activity
+{
+    /**
+     * No-arg constructor for JPA
+     */
+    public Activity ()
+    {}
+
+    /**
+     * Creates an activity with the given name that starts now.
+     */
+    public Activity (String name)
+    {
+        this.name = name;
+        this.start = new Date();
+    }
+
+    public long getId ()
+    {
+        return id;
+    }
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+
+    public Date getStart ()
+    {
+        return start;
+    }
+
+    public void setStart (Date start)
+    {
+        this.start = start;
+    }
+
+    public Date getEnd ()
+    {
+        return end;
+    }
+
+    public void setEnd (Date end)
+    {
+        this.end = end;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    private Date start;
+
+    private Date end;
+}
